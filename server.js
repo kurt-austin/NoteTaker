57fe42758741a7c180b87b6f969f95d93104e917
Kurt Austin <kurt.austin@gmail.com>
2020-08-15 12:22:44 -0500

    Finished Readme

--- server.js
+++ server.js
@@ -21,51 +21,49 @@ app.get("/api/notes", function (req, res) {
   // Reading the string from the file. 
 
   notes = fs.readFileSync("db/db.json", "utf8");
-  
+
   // Parsing the string into an object.
 
 
-    notes = JSON.parse(notes);
+  notes = JSON.parse(notes);
 
   // Returning the string to the browser
-  
-    res.json(notes);
-  
+
+  res.json(notes);
+
 
 });
-
-
 
 
 // POST route for saving a new note
 
 app.post("/api/notes", function (req, res) {
 
-// Reading the string from the file
+  // Reading the string from the file
 
   notes = fs.readFileSync("db/db.json", "utf8");
-  
+
   // Parsing the string into an object.
-    notes = JSON.parse(notes);
+  notes = JSON.parse(notes);
 
-// Adding the ID based on the length of the object.
+  // Adding the ID based on the length of the object.
 
-    req.body.id = notes.length;
-    
- // Pushing the response to the notes array of objects.   
-   
- notes.push(req.body);
+  req.body.id = notes.length;
 
-// Stringifying the object to save it to the file.
-    notes = JSON.stringify(notes);
+  // Pushing the response to the notes array of objects.   
 
-// Writing the new entry to the file.
+  notes.push(req.body);
 
-    fs.writeFileSync("./db/db.json", notes, "utf8", function (err) {
-      if (err) throw err;
-      
-    });
-  
+  // Stringifying the object to save it to the file.
+  notes = JSON.stringify(notes);
+
+  // Writing the new entry to the file.
+
+  fs.writeFileSync("./db/db.json", notes, "utf8", function (err) {
+    if (err) throw err;
+
+  });
+
   // Reading the string file with the new etnry.
 
   notes = fs.readFileSync("db/db.json", "utf8");
@@ -74,54 +72,55 @@ app.post("/api/notes", function (req, res) {
 
   notes = JSON.parse(notes);
 
- // Returning the array to the browser.
+  // Returning the array to the browser.
 
   res.json(notes);
-
-
 
 });
 
 // DELETE route for deleting a note
 app.delete("/api/notes/:id", function (req, res) {
 
-// Reading the string from the file
+  // Reading the string from the file
 
   notes = fs.readFileSync("db/db.json", "utf8");
 
-// Parsing the string into an object.
+  // Parsing the string into an object.
 
   notes = JSON.parse(notes);
 
-// Looping through the length of the array of objects.
+  // Looping through the length of the array of objects.
 
-    for (let i = 0; i < notes.length; i++) {
+  for (let i = 0; i < notes.length; i++) {
 
-// If the loop object is = the chosen one deleted, splice the array by deleting that object.      
-      if (notes[i].id === parseInt(req.params.id)) {
-          notes.splice(i,1);
-      } 
+    // If the loop object is = the chosen one deleted, splice the array by deleting that object.      
+    if (notes[i].id === parseInt(req.params.id)) {
+      notes.splice(i, 1);
+    }
 
-    };
+  };
 
- // Stringifying the new object (minus the deleted note) so that it can be written to the file.   
-    notes = JSON.stringify(notes);
+  // Stringifying the new object (minus the deleted note) so that it can be written to the file.   
+  notes = JSON.stringify(notes);
 
- // Write to the file   
+  // Write to the file   
   fs.writeFile("./db/db.json", notes, "utf8", function (err) {
     // error handling
     if (err) throw err;
   });
 
-// Reading the new file
+  // Reading the new file
+
   notes = fs.readFileSync("db/db.json", "utf8");
 
   // Parsing the string file into an array of object.
-    notes = JSON.parse(notes);
 
- // Pushing the array to the browser.   
-    res.json(notes);
-  
+  notes = JSON.parse(notes);
+
+  // Pushing the array to the browser.   
+
+  res.json(notes);
+
 });
 
 // HTML Routes
@@ -133,7 +132,6 @@ app.get('/notes', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './assets/index.html'))
 })
-
 
 
 app.listen(PORT, () => {
